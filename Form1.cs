606e4fa21d08d39827939c39f7a392d48c4db0dd
Gabrihellzin <bielmuaway91@gmail.com>
2023-02-08 11:21:09 -0300

    Feat: Activate/Deactivate button, trying to macro in task method

--- Form1.cs
+++ Form1.cs
@@ -12,13 +12,38 @@ namespace MacroSharpDX
         [DllImport("user32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
         private static extern Keys GetAsyncKeyState(Keys Tecla);
 
+        //To receive the keys from the keyboard
         [DllImport("user32", CharSet = CharSet.Ansi, EntryPoint = "GetAsyncKeyState", ExactSpelling = true, SetLastError = true)]
         private static extern int GetKeyPress(int key);
 
+        public static bool IsOn { get; set; }
 
         public Form1()
         {
             InitializeComponent();
+            IsOn = false;
+        }
+
+        // Trying the method of macroing in task
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Task task1;
+            if (IsOn == false) { 
+                IsOn = true;
+                task1 = Task.Run(() => MacroActivate(Keys.Space));
+            }
+            else { IsOn = false;}
+
+        }
+
+        public static async Task MacroActivate(Keys keys)
+        {
+            while (IsOn == true) { 
+                Keyboard.KeyDown(keys);
+                await Task.Delay(50);
+                Keyboard.KeyUp(keys);
+                await Task.Delay(70);
+            }
         }
     }
-}
+}    
